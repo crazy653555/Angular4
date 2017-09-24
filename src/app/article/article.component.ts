4769645890531bc513f9699c443a663821c182a7
eason <crazy653555@gmail.com>
2017-09-24 15:56:06 +0800

    練習 Angular 的 @Output() 將資料從子元件透過事件傳值到父元件中

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Article } from '../article';
 
 @Component({
@@ -10,6 +10,9 @@ export class ArticleComponent implements OnInit {
 
   @Input('data')
   item: Article;
+
+  @Output()
+  showTitleEvent = new EventEmitter<string>();
 
   constructor() { }
 
