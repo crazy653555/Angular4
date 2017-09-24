f1ba6da3ccc8cf8c9bea5e5cb1735347209234f9
eason <crazy653555@gmail.com>
2017-09-24 15:17:08 +0800

    練習 Angular 的 @Input() 將資料從父元件傳值到子元件中 (將 @Input 的 item 轉換成強型別)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import {FormsModule} from '@angular/forms';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
